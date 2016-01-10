0d6e83ead51baab55e3077e01595353386a69e58
Александр <aleksanderem@yandex.ru>
2016-01-10 21:07:40 +0300

    Обновление проекта
    - Добавлено событие очистки ресурсов
    - Написан пример работы со строками и файлами

--- EnjoyTheRing0/Main.c
+++ EnjoyTheRing0/Main.c
@@ -1,34 +1,30 @@
-#include "wdm.h"
-
-const PCWSTR DeviceNameStr = L"\\Device\\EnjoyTheRing0";
-const PCWSTR DeviceLinkStr = L"\\??\\EnjoyTheRing0";
-
-NTSTATUS DriverEntry  (IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING RegistryPath);
-VOID     DriverUnload (IN PDRIVER_OBJECT DriverObject);
-NTSTATUS DriverCreate (IN PDEVICE_OBJECT DeviceObject, IN PIRP IORequestPacket);
-NTSTATUS DriverClose  (IN PDEVICE_OBJECT DeviceObject, IN PIRP IORequestPacket);
-NTSTATUS DriverControl(IN PDEVICE_OBJECT DeviceObject, IN PIRP IORequestPacket);
+#include <wdm.h>
+#include "Main.h"
+#include "FilesUtils.h"
+#include "StringsUtils.h"
 
 UNICODE_STRING DeviceName;
 UNICODE_STRING DeviceLink;
-PDEVICE_OBJECT DeviceObject;
 
 // Загрузка драйвера в систему:
 NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING RegistryPath) 
 {
+	PDEVICE_OBJECT DeviceObject;
+
 	NTSTATUS Status = 0;
-	
+
 	DbgPrint("[ETR0]: Loading...");
 
 	// Назначаем события:
 	DriverObject->DriverUnload = DriverUnload;
-	DriverObject->MajorFunction[IRP_MJ_CREATE] = DriverCreate;
-	DriverObject->MajorFunction[IRP_MJ_CLOSE]  = DriverClose;
+	DriverObject->MajorFunction[IRP_MJ_CREATE]  = DriverCreate;
+	DriverObject->MajorFunction[IRP_MJ_CLEANUP] = DriverCleanup;
+	DriverObject->MajorFunction[IRP_MJ_CLOSE]   = DriverClose;
 	DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = DriverControl;
 
 	// Создаём устройство, ассоциирующееся с драйвером:
 	RtlInitUnicodeString(&DeviceName, DeviceNameStr);
-	Status = IoCreateDevice(DriverObject, NULL, &DeviceName, FILE_DEVICE_UNKNOWN, 0, FALSE, &DeviceObject);
+	Status = IoCreateDevice(DriverObject, 0, &DeviceName, FILE_DEVICE_UNKNOWN, 0, FALSE, &DeviceObject);
 
 	if (!NT_SUCCESS(Status)) {
 		DbgPrint("[ETR0]: IoCreateDevice Error!");
@@ -75,15 +71,42 @@ NTSTATUS DriverControl(IN PDEVICE_OBJECT DeviceObject, IN PIRP IORequestPacket)
 	// Определяем возвращаемое количество байт:
 	ULONG ResponseLength = 0;
 
-	// Обрабатываем IOCTL:
+	// Обрабатываем IRP:
 	__try {
-		switch (ControlCode) {
+		// *** Пример работы со строками, файлами и папками ***
 
+		LPWSTR DirectoryPath    = L"C:\\[ETR0] Directory\\";
+		LPWSTR OriginalFileName = L"KernelFile.txt";
+		
+		// Формируем полный путь к файлу:
+		LPWSTR OriginalFilePath;
+		ConcatenateStringsW(DirectoryPath, OriginalFileName, &OriginalFilePath);
+
+		// Создаём рабочую папку:
+		CreateDirectory(DirectoryPath);
+
+		// Создаём файл и записываем в него строку:
+		HANDLE hFile;
+		LPSTR  Text = "Hello, Kernel-World!";
+		CreateEmptyFile(&hFile, OriginalFilePath);
+		WriteFile(hFile, Text, (ULONG)LengthA(Text), NULL, NULL);
+		CloseFile(hFile);
+			
+		// Освобождаем память, занятую под путь к файлу:
+		FreeString(OriginalFilePath);
+
+
+
+		// *** Обработка IOCTL ***
+
+		// Обрабатываем нужный IOCTL, если требуется:
+		switch (ControlCode) {
+			// ...
 		}
+
 	} __except (EXCEPTION_EXECUTE_HANDLER) {
 		DbgPrint("[ETR0]: Exception catched!");
 	}
-
 
 	// Завершение запроса:
 	IORequestPacket->IoStatus.Status = STATUS_SUCCESS;
@@ -97,6 +120,16 @@ NTSTATUS DriverControl(IN PDEVICE_OBJECT DeviceObject, IN PIRP IORequestPacket)
 NTSTATUS DriverCreate(IN PDEVICE_OBJECT DeviceObject, IN PIRP IORequestPacket)
 {
 	DbgPrint("[ETR0]: DriverCreate event!");
+	IORequestPacket->IoStatus.Status = STATUS_SUCCESS;
+	IORequestPacket->IoStatus.Information = 0;
+	IoCompleteRequest(IORequestPacket, IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
+}
+
+// Событие очистки ресурсов драйвера:
+NTSTATUS DriverCleanup(IN PDEVICE_OBJECT DeviceObject, IN PIRP IORequestPacket)
+{
+	DbgPrint("[ETR0]: DriverCleanup event!");
 	IORequestPacket->IoStatus.Status = STATUS_SUCCESS;
 	IORequestPacket->IoStatus.Information = 0;
 	IoCompleteRequest(IORequestPacket, IO_NO_INCREMENT);
--- EnjoyTheRing0/Main.h
+++ EnjoyTheRing0/Main.h
@@ -1,1 +1,18 @@
+/*
+	Книга по программированию драйверов: 
+	 - В.П.Солдатов "Программирование драйверов Windows: http://drp.su/ru/driver_dev/
+*/
+
 #pragma once
+
+#include <wdm.h>
+
+PCWSTR DeviceNameStr = L"\\Device\\EnjoyTheRing0";
+PCWSTR DeviceLinkStr = L"\\??\\EnjoyTheRing0";
+
+NTSTATUS DriverEntry  (IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING RegistryPath);
+VOID     DriverUnload (IN PDRIVER_OBJECT DriverObject);
+NTSTATUS DriverCreate (IN PDEVICE_OBJECT DeviceObject, IN PIRP IORequestPacket);
+NTSTATUS DriverCleanup(IN PDEVICE_OBJECT DeviceObject, IN PIRP IORequestPacket);
+NTSTATUS DriverClose  (IN PDEVICE_OBJECT DeviceObject, IN PIRP IORequestPacket);
+NTSTATUS DriverControl(IN PDEVICE_OBJECT DeviceObject, IN PIRP IORequestPacket);
