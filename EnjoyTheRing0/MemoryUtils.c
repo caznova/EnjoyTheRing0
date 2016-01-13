fd2bfdf13821a1121a4a25139be1068f1557f826
Александр <aleksanderem@yandex.ru>
2016-01-13 19:49:43 +0300

    Поддержка физической памяти

--- EnjoyTheRing0/MemoryUtils.c
+++ EnjoyTheRing0/MemoryUtils.c
@@ -1,11 +1,14 @@
-#include <wdm.h>
-#include <windef.h>
+#include "MemoryUtils.h"
 
 PVOID GetMem(SIZE_T Bytes) {
 	if (Bytes == 0) return NULL;
 	PVOID Memory = ExAllocatePool(NonPagedPool, Bytes);
 	RtlSecureZeroMemory(Memory, Bytes);
 	return Memory;
+}
+
+VOID FreeMem(PVOID Pointer) {
+	ExFreePool(Pointer);
 }
 
 LPWSTR AllocWideString(SIZE_T MaxCharactersCount, BOOL AddNullTerminator, OUT OPTIONAL SIZE_T* AllocatedCharacters) {
@@ -20,10 +23,73 @@ LPSTR AllocAnsiString(SIZE_T MaxCharactersCount, BOOL AddNullTerminator, OUT OPT
 	return GetMem(MaxCharactersCount * sizeof(CHAR));
 }
 
-VOID FreeMem(PVOID Pointer) {
-	ExFreePool(Pointer);
-}
-
 VOID FillChar(PVOID Buffer, SIZE_T BufferSize, UCHAR Char) {
 	RtlFillMemory(Buffer, BufferSize, Char);
+}
+
+PVOID AllocPhysicalMemory(PHYSICAL_ADDRESS PhysicalAddress, SIZE_T NumberOfBytes) {
+	PhysicalAddress.QuadPart += (NumberOfBytes - 1);
+	return MmAllocateContiguousMemory(NumberOfBytes, PhysicalAddress);
+}
+
+PVOID AllocCacheablePhysicalMemory(PHYSICAL_ADDRESS PhysicalAddress, SIZE_T NumberOfBytes, MEMORY_CACHING_TYPE CachingType) {
+	PHYSICAL_ADDRESS HighestAddress;
+	PHYSICAL_ADDRESS BoundaryAddressMultiple;
+	HighestAddress.QuadPart = PhysicalAddress.QuadPart + (NumberOfBytes - 1);
+	BoundaryAddressMultiple.QuadPart = 0;
+	return MmAllocateContiguousMemorySpecifyCache(NumberOfBytes, PhysicalAddress, HighestAddress, BoundaryAddressMultiple, CachingType);
+}
+
+VOID FreePhysicalMemory(PVOID BaseAddress) {
+	MmFreeContiguousMemory(BaseAddress);
+}
+
+PHYSICAL_ADDRESS GetPhysicalAddress(PVOID BaseVirtualAddress) {
+	return MmGetPhysicalAddress(BaseVirtualAddress);
+}
+
+PVOID MapPhysicalMemory(PHYSICAL_ADDRESS PhysicalAddress, SIZE_T NumberOfBytes, MEMORY_CACHING_TYPE CachingType) {
+	return MmMapIoSpace(PhysicalAddress, NumberOfBytes, CachingType);
+}
+
+PVOID MapPhysicalMemoryWithProtect(PHYSICAL_ADDRESS PhysicalAddress, SIZE_T NumberOfBytes, ULONG Protect) {
+	return MmMapIoSpaceEx(PhysicalAddress, NumberOfBytes, Protect);
+}
+
+VOID UnmapPhysicalMemory(PVOID BaseVirtualAddress, SIZE_T NumberOfBytes) {
+	MmUnmapIoSpace(BaseVirtualAddress, NumberOfBytes);
+}
+
+VOID SecureVirtualMemory(PVOID VirtualAddress, SIZE_T NumberOfBytes, ULONG ProbeMode, OUT PHANDLE SecureHandle) {
+	*SecureHandle = MmSecureVirtualMemory(VirtualAddress, NumberOfBytes, ProbeMode);
+}
+
+VOID UnsecureVirtualMemory(HANDLE SecureHandle) {
+	MmUnsecureVirtualMemory(SecureHandle);
+}
+
+BOOLEAN IsAddressValid(PVOID VirtualAddress) {
+	return MmIsAddressValid(VirtualAddress);
+}
+
+BOOL IsUsermodeMemoryReadable(PVOID Address, SIZE_T NumberOfBytes, ULONG RequiredAlignment) {
+	__try {
+		ProbeForRead(Address, NumberOfBytes, RequiredAlignment);
+		return TRUE;
+	} __except (EXCEPTION_EXECUTE_HANDLER) {
+		return FALSE;
+	}
+}
+
+BOOL IsUsermodeMemoryWriteable(PVOID Address, SIZE_T NumberOfBytes, ULONG RequiredAlignment) {
+	__try {
+		ProbeForWrite(Address, NumberOfBytes, RequiredAlignment);
+		return TRUE;
+	} __except (EXCEPTION_EXECUTE_HANDLER) {
+		return FALSE;
+	}
+}
+
+NTSTATUS CopyMemory(PVOID Destination, MM_COPY_ADDRESS Source, SIZE_T NumberOfBytes, ULONG Flags, OUT PSIZE_T BytesCopied) {
+	return MmCopyMemory(Destination, Source, NumberOfBytes, Flags, BytesCopied);
 }
--- EnjoyTheRing0/MemoryUtils.h
+++ EnjoyTheRing0/MemoryUtils.h
@@ -1,10 +1,44 @@
 #pragma once
 
-PVOID GetMem(SIZE_T Bytes);
+#include <ntddk.h>
+#include <windef.h>
 
+// Выделение и освобождение виртуальной NonPaged-памяти из пула:
+PVOID GetMem(SIZE_T Bytes);
+VOID  FreeMem(PVOID Pointer);
+
+// Выделение памяти под ANSI- и Wide-строки:
 LPWSTR AllocWideString(SIZE_T MaxCharactersCount, BOOL AddNullTerminator, OUT OPTIONAL SIZE_T* AllocatedCharacters);
 LPSTR  AllocAnsiString(SIZE_T MaxCharactersCount, BOOL AddNullTerminator, OUT OPTIONAL SIZE_T* AllocatedCharacters);
 
-VOID FreeMem(PVOID Pointer);
-
+// Заполнение буфера символом:
 VOID FillChar(PVOID Buffer, SIZE_T BufferSize, UCHAR Char);
+
+// Выделение непрерывных участков физической памяти:
+PVOID AllocPhysicalMemory(PHYSICAL_ADDRESS PhysicalAddress, SIZE_T NumberOfBytes);
+PVOID AllocCacheablePhysicalMemory(PHYSICAL_ADDRESS PhysicalAddress, SIZE_T NumberOfBytes, MEMORY_CACHING_TYPE CachingType);
+VOID  FreePhysicalMemory(PVOID BaseAddress);
+
+/* Работа с физической памятью:
+     GetPhysicalAddress           - получить физический адрес виртуальной памяти в контексте текущего процесса
+	 MapPhysicalMemory            - отобразить физическую память на виртуальную с настройкой кэширования
+	 MapPhysicalMemoryWithProtect - отобразить физическую память на виртуальную с настройкой прав доступа
+	 UnmapPhysicalMemory          - размапить физическую память
+*/
+PHYSICAL_ADDRESS GetPhysicalAddress          (PVOID BaseVirtualAddress);
+PVOID            MapPhysicalMemory           (PHYSICAL_ADDRESS PhysicalAddress, SIZE_T NumberOfBytes, MEMORY_CACHING_TYPE CachingType);
+PVOID            MapPhysicalMemoryWithProtect(PHYSICAL_ADDRESS PhysicalAddress, SIZE_T NumberOfBytes, ULONG Protect);
+VOID             UnmapPhysicalMemory         (PVOID BaseVirtualAddress, SIZE_T NumberOfBytes);
+
+// "Защитить" память от урезания прав доступа или освобождения (ProbeMode - самый "ограничительный" приемлемый режим):
+VOID SecureVirtualMemory(PVOID VirtualAddress, SIZE_T NumberOfBytes, ULONG ProbeMode, OUT PHANDLE SecureHandle);
+VOID UnsecureVirtualMemory(HANDLE SecureHandle);
+
+// Проверка на валидность адреса:
+BOOLEAN IsAddressValid(PVOID VirtualAddress);
+
+// Проверка на возможность чтения\записи юзермодной памяти и "выровненности" адреса:
+BOOL IsUsermodeMemoryReadable (PVOID Address, SIZE_T NumberOfBytes, ULONG RequiredAlignment);
+BOOL IsUsermodeMemoryWriteable(PVOID Address, SIZE_T NumberOfBytes, ULONG RequiredAlignment);
+
+NTSTATUS CopyMemory(PVOID Destination, MM_COPY_ADDRESS Source, SIZE_T NumberOfBytes, ULONG Flags, OUT PSIZE_T BytesCopied);
